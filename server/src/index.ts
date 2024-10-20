bbf2fec209f399f6457127b790c8d128290db191
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-10-20 22:32:50 +0600

    project api implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+// import router from "./routes.projectRoutes";
+import projectRoutes from "./routes.projectRoutes";
 /* ROUTE IMPORTS */
 
 /* CONFIGURATIONS */
@@ -21,6 +23,9 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
+
+// app.use("/projects", router);
+app.use("/projects", projectRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3013;
