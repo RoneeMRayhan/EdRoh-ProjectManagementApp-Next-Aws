7dca52cf11fff6b917dc87c6b29fd7f3fd5d5a47
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-10-28 22:13:13 +0600

    create project form created but having an issue of date formation

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -6,9 +6,11 @@ import {
   Grid3X3,
   Grid3x3,
   List,
+  PlusSquare,
   Share2,
   Table,
 } from "lucide-react";
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
   activeTab: string;
@@ -16,16 +18,31 @@ type Props = {
 };
 
 function ProjectHeader({ activeTab, setActiveTab }: Props) {
-  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
   return (
     <div className="px-4 xl:px-6">
       {/* MODAL NEW PROJECT */}
+      <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      />
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-        <Header name="Product Design Development" />
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button
+              className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewProjectOpen(true)}
+            >
+              <PlusSquare className="mr-2 h-5 w-5" />
+              New Boards
+            </button>
+          }
+        />
       </div>
 
       {/* TABS */}
-      <div className="dark:border-stroke-dark flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 md:items-center">
+      <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 pb-[8px] pt-2 dark:border-stroke-dark md:items-center">
         <div className="flex flex-1 items-center gap-2 md:gap-4">
           <TabButton
             name="Board"
@@ -63,7 +80,7 @@ function ProjectHeader({ activeTab, setActiveTab }: Props) {
             <input
               type="text"
               placeholder="Search Text"
-              className="dark:border-dark-secondary dark:bg-dark-secondary rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:text-white"
+              className="rounded-md border py-1 pl-10 pr-4 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
             />
             <Grid3X3 className="absolute left-3 top-2 h-4 w-4 text-gray-400 dark:text-neutral-500" />
           </div>
@@ -85,7 +102,7 @@ const TabButton = ({ name, icon, setActiveTab, activeTab }: TabButtonProps) => {
 
   return (
     <button
-      className={`relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 sm:px-2 lg:px-4 dark:text-neutral-500 dark:hover:text-white ${isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""}`}
+      className={`relative flex items-center gap-2 px-1 py-2 text-gray-500 after:absolute after:-bottom-[9px] after:left-0 after:h-[1px] after:w-full hover:text-blue-600 dark:text-neutral-500 dark:hover:text-white sm:px-2 lg:px-4 ${isActive ? "text-blue-600 after:bg-blue-600 dark:text-white" : ""}`}
       onClick={() => setActiveTab(name)}
     >
       {icon}
