c484b6c4fc4846debffca985c6b415b7ea344f07
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-10-28 22:23:38 +0600

    project creation api completed with resolving the date format issue

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -1,6 +1,7 @@
 import Modal from "@/components/Modal";
 import { useCreateProjectMutation } from "@/state/api";
 import React, { useState } from "react";
+import { formatISO } from "date-fns";
 
 type Props = {
   isOpen: boolean;
@@ -17,11 +18,20 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
   const handleSubmit = async () => {
     if (!projectName || !startDate || !endDate) return;
 
+    const formatedStartDate = formatISO(new Date(startDate), {
+      representation: "complete",
+    });
+    const formatedEndDate = formatISO(new Date(endDate), {
+      representation: "complete",
+    });
+
+    console.log(startDate);
+
     await createProject({
       name: projectName,
       description,
-      startDate,
-      endDate,
+      startDate: formatedStartDate,
+      endDate: formatedEndDate,
     });
   };
 
