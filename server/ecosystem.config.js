4384bf841c25faf79d6c65cded837881d8090df1
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-11-02 15:07:19 +0600

    init

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    apps:[
+        {
+            name:"project-management",
+            script:"pnpm",
+            args:"run dev",
+            env:{
+                NODE_ENV:"development",
+            }
+        }
+    ]
+}
